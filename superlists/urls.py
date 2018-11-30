8ce53f9d0b7fd169abeb09e5afe2a0e0740c639b
Brandonyy <hei1hei2hei3@163.com>
2018-11-30 17:53:50 +0800

    from 6.6

--- superlists/urls.py
+++ superlists/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     # re_path(r'^blog/', include('blog.urls')),
     re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    re_path(r'^lists/new$', views.new_list, name='new_list'),
 ]
